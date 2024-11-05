da8fbd4e9804cf3e4950e35bc8f4452092fad758
aidansterling6 <95391945+aidansterling6@users.noreply.github.com>
2024-11-04 20:04:51 -0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,31 @@
-// todo
+const app = document.querySelector<HTMLDivElement>("#app")!;
+interface Button {
+  type: string;
+  innerHTML: string | null;
+  ClickFunction: (event: MouseEvent) => void;
+}
+
+AddHTMLButton({
+  type: "button",
+  innerHTML: "test",
+  ClickFunction: () => {
+    alert("you clicked the button!");
+  },
+});
+
+function AddHTMLElement(
+  type: string,
+  innerHTML: string | null = null,
+): HTMLElement {
+  const tmp = document.createElement(type);
+  app.append(tmp);
+  if (innerHTML !== null) {
+    tmp.innerHTML = innerHTML;
+  }
+  return tmp;
+}
+function AddHTMLButton(button: Button): HTMLElement {
+  const tmp = AddHTMLElement(button.type, button.innerHTML);
+  tmp.addEventListener("click", button.ClickFunction);
+  return tmp;
+}
